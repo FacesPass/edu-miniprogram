94f2289aac70ad8ccb19d497acac4c0abc807be0
JiquanWang99 <951873902@qq.com>
2021-04-24 22:32:11 +0800

    feat: 根据Taro-ui二次封装Swiper组件

---
+++ components/swiper/index.tsx
@@ -0,0 +1,70 @@
+import React from 'react'
+import { Image, View } from '@tarojs/components'
+import { Swiper, SwiperItem } from '@tarojs/components'
+import { FC } from '@tarojs/taro'
+import { memo } from 'react'
+
+import './index.less'
+
+type SwiperItemType = {
+  imgUrl: string
+  url?: string
+}
+
+interface SwiperProps {
+  className?: string
+  style?: React.CSSProperties
+  circular?: boolean
+  autoplay?: boolean
+  indicatorDots?: boolean
+  indicatorColor?: string
+  indicatorActiveColor?: string
+  items: SwiperItemType[]
+}
+
+const IndexSwiper: FC<SwiperProps> = memo(({
+  className,
+  style,
+  circular,
+  autoplay,
+  indicatorColor,
+  indicatorActiveColor,
+  indicatorDots,
+  items }) => {
+
+  if (!items.length) {
+    throw new Error('不能传入一个空的swiperItem数组')
+  }
+
+  return (
+    <Swiper
+      className={className}
+      style={style}
+      indicatorColor={indicatorColor}
+      indicatorActiveColor={indicatorActiveColor}
+      circular={circular}
+      indicatorDots={indicatorDots}
+      autoplay={autoplay}>
+      {
+        items.map((item, index) => {
+          return (
+            <SwiperItem key={index}>
+              <Image className="swiper-img" src={item.imgUrl}></Image>
+            </SwiperItem>
+          )
+        })
+      }
+
+    </Swiper>
+  )
+})
+
+IndexSwiper.defaultProps = {
+  circular: true,
+  indicatorDots: true,
+  autoplay: true,
+  indicatorColor: '#fff',
+  indicatorActiveColor: '#c2c2c2'
+}
+
+export default IndexSwiper
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,13 +1,22 @@
-import { Component } from 'react'
 import { View, Text } from '@tarojs/components'
+import { FC } from '@tarojs/taro'
+
+import Swiper from '../../../components/swiper'
+
+
 import './index.less'
 
-export default class Index extends Component {
-  render() {
-    return (
-      <View className='index'>
-        <Text>哈哈哈哈哈</Text>
-      </View>
-    )
-  }
+const Index: FC = () => {
+  return (
+    <Swiper items={[
+      {
+        imgUrl: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fattach.bbs.miui.com%2Fforum%2F201303%2F16%2F173710lvx470i4348z6i6z.jpg&refer=http%3A%2F%2Fattach.bbs.miui.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1621863901&t=ca2b28a77c6494e4c18be725ace8e98b'
+      },
+      {
+        imgUrl: 'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fattach.bbs.miui.com%2Fforum%2F201312%2F03%2F165526ophx4l6c6ll3cnpl.jpg&refer=http%3A%2F%2Fattach.bbs.miui.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1621863901&t=05c65111a4bb5af3f88121cee608db7f'
+      }
+    ]} />
+  )
 }
+
+export default Index
